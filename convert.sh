822aa0e36de8bda7ed34a31b8c480044c7f85d90
Jordan McKee <77068712+jordanwmckee@users.noreply.github.com>
2022-09-02 20:43:45 -0400

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -12,29 +12,17 @@ set -o pipefail
 VID=${1%.*}
 VID=${VID#*/}
 
-ffmpeg -r 5 -i source-videos/${VID}.mp4 -vf scale=1920:-1 
-all_videos/${VID}_1920_5fps.mp4
-ffmpeg -r 10 -i source-videos/${VID}.mp4 -vf scale=1920:-1 
-all_videos/${VID}_1920_10fps.mp4
-ffmpeg -r 15 -i source-videos/${VID}.mp4 -vf scale=1920:-1 
-all_videos/${VID}_1920_15fps.mp4
-ffmpeg -r 30 -i source-videos/${VID}.mp4 -vf scale=1920:-1 
-all_videos/${VID}_1920_30fps.mp4
+ffmpeg -r 5 -i source-videos/${VID}.mp4 -vf scale=1920:-1 all_videos/${VID}_1920_5fps.mp4
+ffmpeg -r 10 -i source-videos/${VID}.mp4 -vf scale=1920:-1 all_videos/${VID}_1920_10fps.mp4
+ffmpeg -r 15 -i source-videos/${VID}.mp4 -vf scale=1920:-1 all_videos/${VID}_1920_15fps.mp4
+ffmpeg -r 30 -i source-videos/${VID}.mp4 -vf scale=1920:-1 all_videos/${VID}_1920_30fps.mp4
 
-ffmpeg -r 5 -i source-videos/${VID}.mp4 -vf scale=1280:-1 
-all_videos/${VID}_1280_5fps.mp4
-ffmpeg -r 10 -i source-videos/${VID}.mp4 -vf scale=1280:-1 
-all_videos/${VID}_1280_10fps.mp4
-ffmpeg -r 15 -i source-videos/${VID}.mp4 -vf scale=1280:-1 
-all_videos/${VID}_1280_15fps.mp4
-ffmpeg -r 30 -i source-videos/${VID}.mp4 -vf scale=1280:-1 
-all_videos/${VID}_1280_30fps.mp4
+ffmpeg -r 5 -i source-videos/${VID}.mp4 -vf scale=1280:-1 all_videos/${VID}_1280_5fps.mp4
+ffmpeg -r 10 -i source-videos/${VID}.mp4 -vf scale=1280:-1 all_videos/${VID}_1280_10fps.mp4
+ffmpeg -r 15 -i source-videos/${VID}.mp4 -vf scale=1280:-1 all_videos/${VID}_1280_15fps.mp4
+ffmpeg -r 30 -i source-videos/${VID}.mp4 -vf scale=1280:-1 all_videos/${VID}_1280_30fps.mp4
 
-ffmpeg -r 5 -i source-videos/${VID}.mp4 -vf scale=640:-1 
-all_videos/${VID}_640_5fps.mp4
-ffmpeg -r 10 -i source-videos/${VID}.mp4 -vf scale=640:-1 
-all_videos/${VID}_640_10fps.mp4
-ffmpeg -r 15 -i source-videos/${VID}.mp4 -vf scale=640:-1 
-all_videos/${VID}_640_15fps.mp4
-ffmpeg -r 30 -i source-videos/${VID}.mp4 -vf scale=640:-1 
-all_videos/${VID}_640_30fps.mp4
+ffmpeg -r 5 -i source-videos/${VID}.mp4 -vf scale=640:-1 all_videos/${VID}_640_5fps.mp4
+ffmpeg -r 10 -i source-videos/${VID}.mp4 -vf scale=640:-1 all_videos/${VID}_640_10fps.mp4
+ffmpeg -r 15 -i source-videos/${VID}.mp4 -vf scale=640:-1 all_videos/${VID}_640_15fps.mp4
+ffmpeg -r 30 -i source-videos/${VID}.mp4 -vf scale=640:-1 all_videos/${VID}_640_30fps.mp4
